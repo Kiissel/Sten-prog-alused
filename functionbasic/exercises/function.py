c5586b20a9c5c314bf66dcb0eeb655a8637b8041
Sten <sten.kiissel@voco.ee>
2023-10-30 13:42:32 +0200

    function2

---
+++ functionbasic/exercises/function.py
@@ -0,0 +1,28 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    print("I´m inside the function")
+
+# my_name_is(name)
+def my_name_is(name):
+    print("My name is " + name)
+
+# sum_six(num)`
+def sum_six(num):
+    return 6 + num
+
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return a + b
+
+# usd_to_eur()
+def usd_to_eur(num):
+    return (num * 0.8)
+
+my_name_is("Mari")
+print(sum_six(1))
+print(sum_numbers(5, 5))
+print(usd_to_eur(100))
