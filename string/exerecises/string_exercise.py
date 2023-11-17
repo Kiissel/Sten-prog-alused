10e629f3c381477bad408861c3b631eaee16ee31
Sten <sten.kiissel@voco.ee>
2023-11-17 13:17:59 +0200

    sõne

---
+++ string/exerecises/string_exercise.py
@@ -0,0 +1,18 @@
+first_name = "James"
+last_name = "Bond"
+
+
+full_name = first_name + " " + last_name
+
+
+self_description_sentence = (f"My name is {last_name}, {full_name}.")
+
+
+cake = "vahukoormarjadtäidispõhi"
+print(cake.replace("vahukoor", "vahukoor\n").replace("marjad", "marjad\n").replace("täidis", "täidis\n").replace("põhi", "põhi\n"))
+
+
+original_string = "Programming is fun!"
+backwards = original_string[ : :-1]
+every_other = original_string[ : :2]
+first_word_reversed = original_string[10: :-1]
